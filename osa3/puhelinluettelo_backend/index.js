735a6eec813ca6e8408c1b50c39150275ed8d015
inkaam <inka-mannikko@hotmail.com>
2026-04-19 11:29:44 +0300

    osa3, 3.12-3.13

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
-// const cors = require('cors');
+// person  skeema
+const Person = require('./models/person');
 
 const app = express();
-// app.use(cors());
+
 // Middleware json-parser, käsittelee request- ja response -olioita, parsii raakadataa JS-olioiksi bodyyn
 app.use(express.json());
 
 app.use(express.static('dist'));
 
-// app.use(morgan('tiny'));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 morgan.token('posteddata', (request) => {
   return request.method === 'POST' ? JSON.stringify(request.body) : '';
@@ -20,58 +28,39 @@ app.use(
     ':method :url :status :res[content-length] :response-time ms :posteddata',
   ),
 );
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
 
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
 
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook</h1>');
 });
 
+// get all
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
+// get info
 app.get('/info', (request, response) => {
-  const personCount = persons.length;
-  const timeStamp = new Date();
-
-  response.send(
-    `Phonebok has info for ${personCount} people <br/> ${timeStamp}`,
-  );
+  Person.find({}).then((allPersons) => {
+    const personCount = allPersons.length;
+    const timeStamp = new Date();
+    response.send(
+      `Phonebok has info for ${personCount} people <br/> ${timeStamp}`,
+    );
+  });
 });
 
+// get by id
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id); // id meni string muotoon, eikä find löytänyt id:tä, joten laitoin Number()
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
 });
 
 app.delete('/api/persons/:id', (request, response) => {
